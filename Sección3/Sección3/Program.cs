e6076d507e6f422a2717b82a3ab4268aaea249df
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 14:49:18 -0500

    Sección3
    Notas y documentos tomados de las clases de la sección3.

---
+++ Sección3/Sección3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sección3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esta sección es de toma de deciciones
+
+            //El if se maneja igual a Java, adicional si no lleva el bloque de cogido especificado ejecutará automáticamente la siguiente, solo una, linea de código posterior a dónde se encuentr el if.
+
+            //Es importante el tipo de variable que se usa para ahorrar el espacio de memoria reservado por el programa.
+
+            //Al igual que en Java los if se pueden anidar.
+
+            //Escaleras de if-else. Permiten ahorrar procesos haciendo más rápida la ejecución del programa.
+
+            //El switch funciona muy parecido o igual a Java.
+
+        }
+    }
+}
