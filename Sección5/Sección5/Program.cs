f32a8338f4e15f5e10dac78873b4403d3072a23c
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 17:55:47 -0500

    Sección5
    notas de las clases y documentos proporcionados

---
+++ Sección5/Sección5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sección5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Los métodos no cambian mucho, se organizan de la misma manera.
+
+            //Se puede pasar una copia del parametro o el parametro directamente dependiendo si se quiere que se cambie su valor.
+
+            //Existe una palabra clave, 'out' nos permite enviar parametros sin definir para que el método realice la definicíón.
+
+            //Existen las tuplas, son "objetos" que permiten almacenar diferentes tipos de variables en ellos.
+            
+            //Al crear una variable de tipo 'var' el programa le asigna algun tipo según crea el más adecuado.
+
+            //el retorno de un método puede ser una tupla, esto permite retornar varios valores.
+        }
+    }
+}
