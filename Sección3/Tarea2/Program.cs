f8cc3df81707314bb87765bd91d8b9a66d27c395
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 15:02:31 -0500

    Feature/s 3 (#2)
    * Sección3
    
    Notas y documentos tomados de las clases de la sección3.
    
    * Tarea1
    
    Solución de la primera tarea de la seccion 3.
    Hacer un programa que le pida al usuario un número del 1 al 12 y escriba el nombre del mes que corresponde ese número en el calendario. Agregar un case default.
    
    * Tarea2
    
    Solución de la tarea 2 de la sección 3.
    Hacer un programa que le pida al usuario un número y decirle si éste es par o impar.
    
    * Tarea3
    
    Solución de la tarea 3 de la sección 3.
    //Hacer un programa que le diga al usuario el precio que debe pagar por el servicio de estacionamiento de un centro comercial con base en el tiempo que ha permanecido ahí, los primeros 60 minutos: $5.00, las primeras 2 horas $15.00 y de 2 horas en adelante: $40.00.

---
+++ Sección3/Tarea2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    //Hacer un programa que le pida al usuario un número y decirle si éste es par o impar.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaración de variables
+            int num;
+
+            //recolección de información
+            Console.WriteLine("Escriba un número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //Solución del problema
+
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("El numero {0} es par", num);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} es impar", num);
+            }
+        }
+    }
+}
---
+++ Sección3/tarea3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3
+{
+    //Hacer un programa que le diga al usuario el precio que debe pagar por el servicio de estacionamiento de un centro comercial con base en el tiempo que ha permanecido ahí, los primeros 60 minutos: $5.00, las primeras 2 horas $15.00 y de 2 horas en adelante: $40.00.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaración de variables
+            double num;
+
+            //recolección de información
+            Console.WriteLine("Cuantos minutos ha permanecido en el parqueadero?");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            //Solución del problema
+
+            if (num >= 0)
+            {
+                if (num > 60)
+                {
+                    if (num > 120)
+                    {
+                        Console.WriteLine("El saldo a pagar es de $40.00");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El saldo a pagar es de $15.00");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El saldo a pagar es de $5.00");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Número no valido");
+            }
+        }
+    }
+}
