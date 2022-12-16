9d4c742d6798f6b47fb34552d34b7d1bf9638b33
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 12:03:01 -0500

    Tarea2
    Solución a la segunda tarea de la seción 2.
    Hacer un programa que transfome de grados Centigrados a grados Fahrenheit

---
+++ Sección2/Sección2/Tarea2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2._2
+{
+    //Hacer un programa que transfome de grados Centigrados a grados Fahrenheit
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Declaración de variables
+            double cel;
+
+            //recopilación de la información
+            Console.WriteLine("Cual es la cantidad en grados Celcius?");
+            cel = Convert.ToDouble(Console.ReadLine());
+
+            //Resolviendo el problema 
+
+            Console.WriteLine("El equivalente en grados fahrenheit son: {0}", (cel * 9 / 5) + 32);
+            Console.ReadLine();
+
+        }
+    }
+}
