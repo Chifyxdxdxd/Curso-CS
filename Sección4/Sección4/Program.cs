fb26432b4c6c0fa97bb712b19dccd8fd1082fabb
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 16:26:58 -0500

    Sencción4
    Notas y Documentos de la sección 4

---
+++ Sección4/Sección4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sección4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //operadores de incremento iguales a Java, siempre comparo con Java porque es el lengueaje en el que más me siento cómodo hasta ahora.
+
+            //El for, while y do-while funcionan igual. Lo único diferente es cómo usa el iterador, lo crea por fuera del ciclo.
+            
+            //Hace falta el uso de un ciclo, esta propuesto para una parte más adelantada del curso
+        }
+    }
+}
