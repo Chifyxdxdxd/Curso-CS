807605cb591eb96eb0a355186b9c617502c86e56
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-16 17:20:29 -0500

    Tarea2
    Solución de la tarea 2 de la sección 4.
    Hacer un programa que calcule los números primos que existen entre el 1 y el 100.

---
+++ Sección4/Tarea2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        //Hacer un programa que calcule los números primos que existen entre el 1 y el 100.
+        static void Main(string[] args)
+        {
+            //Declaración de variables
+            int numDiv = 0;
+
+            for (int num = 2; num <= 100; num++)//no es necesario crear el iterador fuera del cliclo
+            {
+
+                for (int div = 1; div <= num; div++)
+                {
+                    if ((num % div) == 0)
+                    {
+                        numDiv += 1;
+                    }
+                }
+
+                if (numDiv <= 2)
+                {
+                    Console.WriteLine("numero primo: {0}", num);
+                }
+                numDiv = 0;
+            }
+        }
+    }
+}
