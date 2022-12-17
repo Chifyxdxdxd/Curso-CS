b60be176781114de6aaaf7f990a87443bb59b2bd
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-17 09:48:43 -0500

    sección6
    notas y documentos de la sección6

---
+++ Sección6/Sección6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sección6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //En C# los arreglos son considerados matrices unidimencionales.
+
+            //El metodo Length permite saber cuantos elementos tiene una matriz independiente de sus dimenciones.
+
+            //Las matrices escalonadas son matrices de varias dimenciones. Una matris escalonada no necesita ser tener el mismo tamaño en sus dimenciones (ejemplo en el curso).
+
+            //La forma de acceder a los valores de cada uno de los de la matriz es igual a Java, dos ciclos for anidados.
+
+            //Asi como se pueden retornar tuplas o enviar como parametros, pasa lo mismo con las matrices, se pueden enviar matrices como parametros.
+        }
+    }
+}
