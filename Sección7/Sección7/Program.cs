2efea44ce4f024a45bb99de9b812e522a1006a1e
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2022-12-17 13:15:21 -0500

    tarea
    Agrego la tarea planteada para la sección 7, me basé en los diagramas que el profesor brinda.

--- Sección7/Sección7/Program.cs
+++ Sección7/Sección7/Program.cs
@@ -10,6 +10,94 @@ namespace Sección7
     {
         static void Main(string[] args)
         {
+            /*Entre los miembros de la clase tenemos
+             * Datos:
+             *  - Campos, son variables que se ubican en la clase, no en los métodos
+             *  - Constantes
+             * Comportamiento:
+             *  - Propiedades
+             *  - Métodos
+             *  - Constructores
+             *  - Finalizadores
+             */
+
+            /* Además de los miembros de clase tenemos los tipos de clase.
+             * De valor:
+             *  - Estructura 
+             *  - Enumeración
+             *  - Némericos integrales
+             *  - Númericos de tipo flotante
+             *  - Booleanos
+             *  - Char
+             *  - Tuplas
+             * De referencia:
+             *  - Clases
+             *  - Interfaces
+             *  - Delegados
+             *  - Record
+             */
+
+            //Las clases tienen niveles de accesibilidad, las cuales permiten usar o madificar los valores o usa los metodos.
+            /* Entre los niveles de accesibilidiad tenemos 4 moddificadores y 6 niveles de accesibilidad.
+             * Modificadores:
+             *  - Public
+             *  - Protected
+             *  - internal
+             *  - Private
+             * Niveles de accesibilidad:
+             *  - Public
+             *  - Protected
+             *  - internal
+             *  - Private
+             *  - Protected internal
+             *  - Private Protected
+             */
+
+            //Para este curso veremos private y publico, por ahora
+
+            //El uso de static cambia los valores del objeto a la clase, es decir, ese método no es especifico de los objetos y estos no pueden usarlos, es la clase la que debe usar los métodos estaticos
+
+            // Hay dos tipos de memoria, stack y heap. Ejemplo de las cajas amontonadas o puestas en forma de torre.
+
+            // el uso de metodos getters an setters para modificar los campos de un objeto y mantener la privacidad de este.
+
+            // Los metodos get y set si cambias en su escritura comparado con Java. Es posible poner el get y set en el mismo método de la clase. 
+
+            /* MIEMBROS EN FORMA DE EXPRESIÓN 
+             * Es una forma de escribir los set y get de una clase. 
+             * de la forma normal tendremos que usar:
+             * 
+             *          set {campo = value;} 
+             *          get {return campo;}
+             *          
+             * Mientras que de la forma de expresión usaremos:
+             *          
+             *          set => campo = value;
+             *          get => campo;
+             *          
+             * solo hay una expresión para modificar es mejor usar esta forma, mientras que si hay que modificar varios campos es mejor usar la primera forma (bloque de codigo) para que no marque error.
+             */
+            
+            // El método ToString transforma un objeto en una cadena de texto. Al igual que en Java existe el override, pero se usa diferente, no se usa con un @ y encima dle método.
+
+            /* en Java
+             *          @override 
+             *          privacidad tipo nombre (parametros){
+             *              bloque de codigo
+             *              return; de ser necesario
+             *          }
+             * 
+             * en C#
+             *          privacidad override tipo nombre (parametros)
+             *          {
+             *              bloque de código
+             *          }
+             */
+            
+            //La sobre carga de métodos no cambia
         }
+
+        
+
     }
 }
---
+++ Sección7/Tarea/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //campos
+            double monto, saldoInicial;
+            int opcion = 0;
+            string nombre, apellidos, direccion, rfc;
+
+
+            //mensaje de cuenta nueva
+            Console.WriteLine("Está a punto de crear una cuenta nueva, por favor precione cualquier tecla para continuar");
+            Console.ReadKey();
+
+            //creando una cuenta nueva
+            Console.WriteLine ("\nIngrese la información que se solicita a continuación: \nNombre:");
+            nombre = Console.ReadLine ();
+            Console.WriteLine("Apellidos: ");
+            apellidos = Console.ReadLine ();
+            Console.WriteLine("Dirección: ");
+            direccion = Console.ReadLine ();
+            Console.WriteLine("RFC: ");
+            rfc = Console.ReadLine ();
+            Console.WriteLine("Ingrese su deposito inicial");
+            saldoInicial = Convert.ToDouble(Console.ReadLine());
+
+            //mensajes de confirmación
+            CuentaBancaria cuenta1 = new CuentaBancaria(nombre,apellidos, direccion, rfc, saldoInicial);
+            Console.WriteLine("Felicidades, su cuenta ha sido creada con exito, presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+            //mostrar opciones de menú
+            do
+            {
+                Console.WriteLine("\n1. Deposito\n2. Retiro\n3. Consultar Saldo\n4. Mostrar Información de la cuenta\n5. Salir");
+                opcion = Convert.ToInt16(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("cuanto desea ingresar?");
+                        monto= Convert.ToDouble(Console.ReadLine());
+                        cuenta1.Deposito(monto);
+                        cuenta1.ConsultaSaldo();
+                        break;
+                    case 2:
+                        Console.WriteLine("cuanto desea retirar?");
+                        monto = Convert.ToDouble(Console.ReadLine());
+                        cuenta1.Retiro(monto);
+                        cuenta1.ConsultaSaldo();
+                        break;
+                    case 3:
+                        cuenta1.ConsultaSaldo();
+                        break;
+                    case 4:
+                        Console.WriteLine(cuenta1.ToString());
+                        break;
+
+                }
+
+            } while (opcion>=1 && opcion<=4);
+                
+            
+            
+
+
+        }
+    }
+
+    public class CuentaBancaria
+    {
+        //campos
+        private string nombre, apellidos, direccion, rfc;
+        private double saldo;
+
+        //Métodos
+        public CuentaBancaria(string nombre, string apellidos, string direccion, string rfc, double saldo)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.direccion = direccion;
+            this.rfc = rfc;
+            this.saldo = saldo;
+        }
+
+        public double Deposito (double monto)
+        {
+            this.saldo+= monto;
+            return saldo;
+        }
+
+        public double Retiro(double monto)
+        {
+            if (this.saldo > monto) 
+            {
+                this.saldo -= monto;
+            }
+            else
+            {
+                Console.WriteLine("El saldo es insuficiente");
+            }
+            return saldo;
+        }
+
+        public void ConsultaSaldo()
+        {
+            Console.WriteLine("El saldo de tu cuenta es: {0}", this.saldo);
+        }
+
+        public override string ToString()
+        {
+            string mensaje = "";
+            mensaje = "\nTitular: " + this.nombre + " " + this.apellidos + "\nRFC:" + this.rfc + "\nDirección: " + this.direccion + "\nSaldo: $" + this.saldo;
+            return mensaje;
+        }
+    }
+}
