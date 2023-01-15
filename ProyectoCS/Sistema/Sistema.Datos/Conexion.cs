83ba6e61bc3b9b2d7395cc9afe264478bcfce87d
Santiago Rojas <114033327+Chifyxdxdxd@users.noreply.github.com>
2023-01-15 15:39:02 -0500

    commit 1,
    Los modulos 2 a 5 del curso en el cual se plantea el proyecto.

---
+++ ProyectoCS/Sistema/Sistema.Datos/Conexion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class Conexion
+    {
+        private string @base;
+        private string servidor;
+        private string usuario;
+        private string clave;
+        private bool seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.@base = "dbsistema";
+            this.servidor = "DESKTOP-N7SQCT8";
+            this.usuario = "Steve";
+            this.clave = "pony123"; 
+            this.seguridad = true;
+        }
+
+        public SqlConnection crearConexion()
+        {
+            SqlConnection cadena = new SqlConnection();
+            try
+            {
+                cadena.ConnectionString = "Server={$this.servidor};Database={$this.@base};";
+                if (this.seguridad)
+                {
+                    cadena.ConnectionString+= "Integrated Security = SSPI;";
+                }
+                else
+                {
+                    cadena.ConnectionString += "User Id={$this.usuario};Password={$this.clave};";
+                }
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw ex;
+            }
+            return cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null) 
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
---
+++ ProyectoCS/Sistema/Sistema.Negocio/NCategoria.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Sistema.Datos;
+using Sistema.Entidades;
+
+namespace Sistema.Negocio
+{
+    public class NCategoria
+    {
+
+        public static DataTable Listar() 
+        { 
+            DCategoria datos = new DCategoria();
+            return datos.Listar(); 
+        }
+
+        public static DataTable Buscar(string val)
+        {
+            DCategoria datos = new DCategoria();
+            return datos.Buscar(val);
+        }
+
+        public static string Insertar(string nombre, string descripcion)
+        {
+            DCategoria datos = new DCategoria();
+            Categoria obj = new Categoria();
+            obj.nombre = nombre;
+            obj.descripcion = descripcion;
+            return datos.Insertar(obj);
+        }
+
+        public static string Actializar(int id, string nombre, string descripcion)
+        {
+            DCategoria datos = new DCategoria();
+            Categoria obj = new Categoria();
+            obj.idCategoria = id;
+            obj.nombre = nombre;
+            obj.descripcion = descripcion;
+            return datos.Insertar(obj);
+        }
+
+        public static string Eliminar(int id)
+        {
+            DCategoria datos = new DCategoria();
+            return datos.Eliminar(id);
+        }
+
+        public static string Actualizar(int id)
+        {
+            DCategoria datos = new DCategoria();
+            return datos.Activar(id);
+        }
+
+        public static string Desactualizar(int id)
+        {
+            DCategoria datos = new DCategoria();
+            return datos.Desactivar(id);
+        }
+    }
+}
